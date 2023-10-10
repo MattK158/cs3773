6e77febb429f9329478df0b74fe1a1c55cc051bd
marknagieb <marknagieb@gmail.com>
2023-10-10 18:36:57 -0500

    Added Features
    Added Home Page and Navbar

--- swe-app/src/App.js
+++ swe-app/src/App.js
@@ -2,29 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import SignUp from './SignUp'; 
-import SignIn from './SignIn';
-import Checkout from './Checkout';
+import SignUp from './components/SignUp'; 
+import SignIn from './components/SignIn';
+import Checkout from './components/Checkout';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-          <Link to="/signup">
-            <button>Go to SignUp</button>
-          </Link>
-          <Link to="/signin">
-            <button>Go to SignIn</button>
-          </Link>
-          <Link to="/checkout">
-            <button>Go to Checkout</button>
-          </Link>
+      <Navbar />
+      <Home />
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
-      </div>
     </Router>
   );
 }
---
+++ swe-app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './Home.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <h1>Welcome!</h1>
+      <p>What are you looking for?</p>
+      <div className='hero-btns'>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
