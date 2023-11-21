ad53ce75d5c82c5a0f403720f0793312572faf62
camguo <83855718+camguo@users.noreply.github.com>
2023-11-21 12:34:21 -0600

    11/21 order sum page

--- src/swe-app/src/pages/OrderSummary.js
+++ src/swe-app/src/pages/OrderSummary.js
@@ -4,6 +4,7 @@ import './Checkout.css';
 import { useUser } from '../UserContext';
 import { containerClasses } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import './OrderSummary.css'
 
 const OrderSummary = () => {
     const { user } = useUser();
@@ -41,22 +42,29 @@ const OrderSummary = () => {
 
     return (
         <div className='ordersummary-main'>
-            <div className='ordersummary-header'>
-                <h2>Order successfully placed!</h2>
-                <h1>Order Summary</h1>
-                <h1>{"Order ID: " + mostRecentOrder.id}</h1>
-                <h1>{"Order Pickup Time: " + mostRecentOrder.pickupTime}</h1>
-            </div>
-            <div className='ordersummary-body'>
-                {mostRecentOrder.orderSummaryDto.orderItemsInCart.map((item) => (
-                    <div className='item' key={item.id}>
-                        <h3 className='item-name'>{item.name}</h3>
-                        <p className='item-price'>{"$" + item.priceAtPurchase}</p>
+            <div className='ordersummary-inner'>
+                <div className='ordersummary-header'>
+                    <h1>Thank you for your order</h1>
+                    <div className='order-num'>
+                        <h2>{"Order number:"}</h2>
+                        <p>{mostRecentOrder.id}</p>
                     </div>
-                ))}
-            </div>
-            <div className="cart-summary">
-                <p>Order Total: ${mostRecentOrder.orderSummaryDto.totalPrice}</p>
+                    <div className='pickup-time'>
+                        <h2>{"Pickup Time:"}</h2>
+                        <p>{mostRecentOrder.pickupTime}</p>
+                    </div>
+                </div>
+                <div className='ordersummary-body'>
+                    {mostRecentOrder.orderSummaryDto.orderItemsInCart.map((item) => (
+                        <div className='item-display-box' key={item.id}>
+                            <h4 className='item-name'>{item.name}</h4>
+                            <p className='item-price'>{"$" + item.priceAtPurchase}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="cart-summary">
+                    <h3>Total: ${mostRecentOrder.orderSummaryDto.totalPrice}</h3>
+                </div>
             </div>
         </div>
     );
